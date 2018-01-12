72c10b0d5652ecbd2217a2d69b4646fbe7908835
Bruno Lima <bg.lima@hotmail.com>
2018-01-11 23:01:17 -0300

    Added resize function in order to keep a specified max dimension size

--- src/dataset_utils.py
+++ src/dataset_utils.py
@@ -10,15 +10,15 @@ import cv2
 import numpy as np
 import imutils as iu
 
-INPUT_PATHS = ['../db/training004/flowerpot/' ]
-            #'../db/training004/tire/'   
+INPUT_PATHS = [ '../db/training004/flowerpot/', 
+            '../db/training004/tire/' ]
             #'../db/training004/flowerpot/',
             #'../db/training004/bottle/']    
-AUGMENT_OPTIONS = [ 'ROTATION', 'SHEAR' ]
+AUGMENT_OPTIONS = [ 'ROTATION' ]
 VALID_TYPES = ('.jpg', '.gif', '.png', '.tga')
 TARGET_SIZE = (300, 300)
 
-
+# Augmentation process for each iamge
 def process( aug_type, img_name, img, out_dir ):   
     # ROTATION
     if aug_type == 'ROTATION':
@@ -52,7 +52,41 @@ def process( aug_type, img_name, img, out_dir ):
         raise Exception('%s augmentation command not known.')
 
     print('All right!')
-    
+
+# Override original images with TARGET_SIZE images, keeping aspect ratio
+# Does not consider annotations
+def resize_datasets():
+    # Iterate over folders
+    for input_dir in INPUT_PATHS:
+        img_dir = os.path.join(input_dir, 'images/')            
+        
+    # Check if folders exist
+    if not os.path.exists(img_dir):
+        raise Exception('%s dir not found.'% (img_dir) ) 
+
+    # Load names from img directory            
+    for img_name in os.listdir(img_dir):
+        # Load image from path
+        img_path = os.path.join(img_dir, img_name)
+        img = cv2.imread(img_path)
+
+         # Resize the image and override it
+        width, height = img.shape[1], img.shape[0]
+        aspect_ratio = float(width) / float(height)
+        
+        # Resize max dimension to 300, keeping ratio
+        if width > height:
+            new_width = TARGET_SIZE[0]
+            new_height = int( float(new_width) / float(aspect_ratio) )
+        else:
+            new_height = TARGET_SIZE[1]                                
+            new_width = int( float(new_height) * float(aspect_ratio) )
+            
+        img = cv2.resize(img, (new_width, new_height), interpolation = cv2.INTER_CUBIC )
+        print('Resizing %s and saving' % img_name)
+        cv2.imwrite(img_dir+img_name, img)
+
+# Process each image and store its results in INPUT_PATH/augmented folder.
 def augment_datasets():
     # Iterate over folders
     for input_dir in INPUT_PATHS:
@@ -78,8 +112,7 @@ def augment_datasets():
             
             # Check if its a valid image
             if (img is not None) and (img_path.lower().endswith(VALID_TYPES)):
-                # First of all, resize the image and write to output folder
-                img = cv2.resize(img, TARGET_SIZE, interpolation = cv2.INTER_CUBIC)
+                # Save current image to output folder
                 cv2.imwrite(out_dir+img_name, img)
             
                 # Augmentation option from each image goes here
