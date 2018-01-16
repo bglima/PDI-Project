BUG: Noise needs to come before rotation. Wrong BB otherwise

src/dataset_utils.py
@@ -19,7 +19,7 @@ INPUT_PATHS = [
             #'../db/training007/tire/', 
             #'../db/training007/flowerpot/' 
             ]
-AUGMENT_OPTIONS = [ 'ROTATION' ]
+AUGMENT_OPTIONS = [ 'NOISE', 'ROTATION' ]
 VALID_TYPES = ('.jpg', '.gif', '.png', '.tga')
 TARGET_SIZE = (300, 300)
 OX, OY = TARGET_SIZE[0] // 2, TARGET_SIZE[1] // 2
@@ -105,7 +105,7 @@ def get_bb_from_ann( ann_tree ):
     ymax = ann_tree.getroot().find('object/bndbox/ymax').text
     return int(xmin), int(ymin), int(xmax), int(ymax)
 
-# Rotate arround origin counterclockwise, where
+# Rotate arround origin counterclockwise
 #   px, py = point
 #   ox, oy = origin
 #   angle = radians    
@@ -126,12 +126,50 @@ def rotate_point( px, py, ox, oy, angle  ):
     # Resulting point
     return org_mat + np.dot(rot_mat, dif_mat)
 
+# Shear point
+#   px, py = point
+#   ox, oy = origin
+#   k = shear_factor    
+def shear_point( px, py, ox, oy, k, horizontal=True  ):
+    # Defining difference between point and origin, and angles
+    dx = px - ox
+    dy = py - oy
+    
+    # Origin matrix
+    org_mat = np.array([ox, oy]).reshape(2, 1)
+    # Offset matrix
+    dif_mat = np.array([dx, dy]).reshape(2, 1)
+    # Shear matrix
+    if horizontal:
+        rot_mat = np.array([[ 1, k ], [ 0, 1 ]])
+    else:
+        rot_mat = np.array([[ 1, 0 ], [ k, 1 ]])
+    
+    # Resulting point
+    return org_mat + np.dot(rot_mat, dif_mat)
+
 
 # Get rotated bounding box
 def rotate_bb( xmin, ymin, xmax, ymax, angle ):
     p1x, p1y = rotate_point( xmin, ymin, OX, OY, angle )
     p2x, p2y = rotate_point( xmax, ymax, OX, OY, angle )
     return int(min(p1x, p2x)), int(min(p1y, p2y)), int(max(p1x, p2x)), int(max(p1y, p2y))
+
+# Get sheared bounding box
+def shear_bb( xmin, ymin, xmax, ymax, k, horizontal ):
+    p1x, p1y = shear_point( xmin, ymin, 0, 0, k, horizontal)
+    p2x, p2y = shear_point( xmax, ymax, 0, 0, k, horizontal )
+    nx_min, ny_min = int(min(p1x, p2x)), int(min(p1y, p2y))
+    nx_max, ny_max = int(max(p1x, p2x)), int(max(p1y, p2y))
+    
+    # Check if out of bounds
+    if nx_min < 0: nx_min = 0
+    if ny_min < 0: nx_min = 0
+    if nx_max > TARGET_SIZE[0]: nx_max = TARGET_SIZE[0]
+    if ny_max > TARGET_SIZE[1]: ny_max = TARGET_SIZE[1]
+    
+    # Return values
+    return nx_min, ny_min, nx_max, ny_max
 
 # Augmentation process for each iamge
 def process( aug_type, img_name, img, ann_tree, out_dir, ann_out_dir ): 
@@ -166,6 +204,7 @@ def process( aug_type, img_name, img, ann_tree, out_dir, ann_out_dir ):
             # Image processing - Horizontal           
             shear_matrix_h = np.array([[1,shear_factor, 0], [0, 1, 0] ])
             img_out = cv2.warpAffine(img, shear_matrix_h, TARGET_SIZE)
+            nx_min, ny_min, nx_max, ny_max = shear_bb( xmin, ymin, xmax, ymax, shear_factor, True ) 
             # New image path
             img_out_name = img_raw_name + 'shearH' + str(shear_factor) 
             img_out_path = out_dir + img_out_name + '.jpg'
@@ -175,11 +214,12 @@ def process( aug_type, img_name, img, ann_tree, out_dir, ann_out_dir ):
             print('Writing %s ...' % (img_out_path) ) 
             print('XML being saved to %s...\n\n' % ann_path )
             cv2.imwrite(img_out_path, img_out)
-            save_to_ann(ann_tree, ann_path, img_out_path, 0, 0, 0, 0)
+            save_to_ann(ann_tree, ann_path, img_out_path, nx_min, ny_min, nx_max, ny_max)
             
             # Image Processing - Vertical
             shear_matrix_v = np.array([[1, 0, 0], [shear_factor, 1, 0]])
             img_out = cv2.warpAffine(img, shear_matrix_v, TARGET_SIZE)
+            nx_min, ny_min, nx_max, ny_max = shear_bb( xmin, ymin, xmax, ymax, shear_factor, False ) 
             # New image path
             img_out_name = img_raw_name + 'shearV' + str(shear_factor)
             img_out_path = out_dir + img_out_name  + '.jpg'
@@ -189,17 +229,20 @@ def process( aug_type, img_name, img, ann_tree, out_dir, ann_out_dir ):
             print('Writing %s ...' % (img_out_path) ) 
             print('XML being saved to %s...\n\n' % ann_path )
             cv2.imwrite(img_out_path, img_out)
-            save_to_ann(ann_tree, ann_path, img_out_path, 0, 0, 0, 0)
+            save_to_ann(ann_tree, ann_path, img_out_path, nx_min, ny_min, nx_max, ny_max)
             
-    # SALT AND PEPPER
+    # GAUSSIAN NOISE
     elif aug_type == 'NOISE':     
         # Creating noise based on image
-        gaussian_noise = img.copy()
-        m = (20, 20, 20)
-        s = (20, 20, 20)
-        cv2.randn(gaussian_noise, m, s)   # Mean 0 and sigma 150
+        row,col,ch = img.shape
+        mean = 0
+        var = 1000.0
+        sigma = var**0.5
+        gauss = np.random.normal(mean,sigma,(row,col,ch))
+        gauss = gauss.reshape(row,col,ch)
         # Adding noise to original image
-        img_out = img + gaussian_noise
+        img_out = img + gauss
+        
         # New image path
         img_out_name = img_raw_name + 'noise'
         img_out_path = out_dir + img_out_name + '.jpg'
@@ -208,7 +251,7 @@ def process( aug_type, img_name, img, ann_tree, out_dir, ann_out_dir ):
         # Saving
         print('Writing %s ...' % (img_out_path) ) 
         cv2.imwrite(img_out_path, img_out)
-        save_to_ann(ann_tree, ann_path, img_out_path, 0, 0, 0, 0)
+        save_to_ann(ann_tree, ann_path, img_out_path, xmin, ymin, xmax, ymax)
     else:
         raise Exception('%s augmentation command not known.')
     print('All right!')